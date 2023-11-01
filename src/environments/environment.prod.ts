8eda5605d5634843ce1702c6fcdb2c5eb3d24417
Mahmoud Kamal <ma7moud.kamal222@email.com>
2023-11-01 22:19:33 +0200

    Fix: Staging URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
-import { Url } from "src/app/modules/shared/_models/url.enum";
+import { Url } from 'src/app/modules/shared/_models/url.enum';
 
 export const environment = {
   production: true,
-  backendUrl: Url.prod,
+  // backendUrl: Url.prod,
+  backendUrl: Url.dev,
 };
